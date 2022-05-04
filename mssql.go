8318bf6a325aea90eac9ffd57109c6b9d0059904
Armen Arsakian <aarsakian@gmail.com>
2022-05-04 23:58:17 +0300

    moved code to modules

--- mssql.go
+++ mssql.go
@@ -1,104 +1,46 @@
+//page size 8KB
+//extend = 8 contiguous pages
+//data row offsets at the end of the page sequentially in reverse byte
+//offset from the beginning of the page
+
+/*GAM and SGAM pages. The first GAM page is always the third page in
+the data file (page number 2). The first SGAM page is always the fourth page in the data file (page number 3).
+The next GAM and SGAM pages appear every 511,230 pages in the data files, which allows SQL Server to navigate
+through them quickly when needed.*/
+/*Every PFS page tracks 8,088 pages, or about 64 MB of data space. It is always the second page (page 1) in
+the file and every 8,088 pages thereafter. Every pffstatus byte tracks info about a page*/
+
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"errors"
+	"MSSQLParser/page"
+	"flag"
 	"fmt"
 	"os"
-	"reflect"
 )
 
-type ColData []byte
+type Database []page.Page
 
-type Row struct {
-	StatusA               uint8  //1
-	StatusB               uint8  //2
-	NofColsOffset         uint16 //3-4
-	ID                    []byte //5
-	NofCols               uint16
-	NullBitmap            uint8
-	NumberOfVarLengthCols uint16
-	ColOffsets            []uint16 //offset where data for varying length column ends
-	ColsData              []ColData
-}
+func (db Database) ShowStats() {
 
-func Unmarshal(data []byte, v interface{}) error {
-	idx := 0
-	structValPtr := reflect.ValueOf(v)
-	structType := reflect.TypeOf(v)
-	if structType.Elem().Kind() != reflect.Struct {
-		return errors.New("must be a struct")
-	}
-	for i := 0; i < structValPtr.Elem().NumField(); i++ {
-		field := structValPtr.Elem().Field(i) //StructField type
-		switch field.Kind() {
+	allocMap := db[2].GetAllocationMaps()
+	allocMap.ShowAllocations()
 
-		case reflect.Uint8:
-			var temp uint8
-			binary.Read(bytes.NewBuffer(data[idx:idx+1]), binary.LittleEndian, &temp)
-			field.SetUint(uint64(temp))
-			idx += 1
-		case reflect.Uint16:
-			var temp uint16
-			binary.Read(bytes.NewBuffer(data[idx:idx+2]), binary.LittleEndian, &temp)
-			field.SetUint(uint64(temp))
-			idx += 2
-		case reflect.Uint32:
-			var temp uint32
-			binary.Read(bytes.NewBuffer(data[idx:idx+4]), binary.LittleEndian, &temp)
-			field.SetUint(uint64(temp))
-			idx += 4
-
-		case reflect.Array:
-			idx += field.Len()
-		case reflect.Slice:
-			name := structType.Elem().Field(i).Name
-			if name == "ID" {
-				len := structValPtr.Elem().FieldByName("NofColsOffset").Uint()
-
-				field.SetBytes(data[idx:len])
-				idx += field.Len()
-			} else if name == "ColOffsets" {
-				colIdx := 0
-				var temp uint16
-				nofCols := int(structValPtr.Elem().FieldByName("NumberOfVarLengthCols").Uint())
-				colsOffsets := reflect.ValueOf(make([]uint16, 0))
-
-				for colIdx < nofCols { // loop over varlen cols
-					binary.Read(bytes.NewBuffer(data[idx:idx+2]), binary.LittleEndian, &temp)
-					colsOffsets = reflect.Append(colsOffsets, reflect.ValueOf(temp))
-					colIdx++
-					idx += 2
-				}
-				field.Set(reflect.Value(colsOffsets))
-			} else if name == "ColsData" {
-				colIdx := 0
-				nofCols := int(structValPtr.Elem().FieldByName("NumberOfVarLengthCols").Uint())
-				colsData := reflect.ValueOf(make([]ColData, 0)) //allocate
-				for colIdx < nofCols {
-					// get value from slice
-					endingOffset := int(structValPtr.Elem().FieldByName("ColOffsets").Slice(colIdx, colIdx+1).Interface().([]uint16)[0])
-					colsData = reflect.Append(colsData, reflect.ValueOf(data[idx:endingOffset]))
-					colIdx++
-
-				}
-				field.Set(colsData)
-			}
-		case reflect.Pointer:
-
-		}
-
-	}
-	return nil
 }
 
 func main() {
-	file, err := os.Open(os.Args[1]) //
 
+	PAGELEN := 8192
+
+	inputfile := flag.String("db", "", "absolute path to the MDF file")
+	selectedPage := flag.Int("page", -1, "select a page to start parsing")
+
+	flag.Parse()
+
+	file, err := os.Open(*inputfile) //
 	if err != nil {
 		// handle the error here
-		fmt.Printf("err %s for reading the MFT ", err)
+		fmt.Printf("err %s for reading the mdf file ", err)
 		return
 	}
 
@@ -110,19 +52,28 @@ func main() {
 
 	defer file.Close()
 
-	bs := make([]byte, 39) //byte array to hold MFT entries
-
-	for i := 0; i <= int(fsize.Size()); i += 39 {
+	bs := make([]byte, PAGELEN) //byte array to hold one PAGE 8KB
+	var database Database
+	for i := 0; i <= int(fsize.Size()); i += PAGELEN {
 		_, err := file.ReadAt(bs, int64(i))
-		// fmt.Printf("\n I read %s and out is %d\n",hex.Dump(bs[20:22]), readEndian(bs[20:22]).(uint16))
+
 		if err != nil {
 			fmt.Printf("error reading file --->%s", err)
 			return
 		}
 
-		var row Row
-		Unmarshal(bs, &row)
-		fmt.Printf("%d", row.ColsData[0])
+		if *selectedPage != -1 && (i/PAGELEN < *selectedPage || i/PAGELEN > *selectedPage) {
+			continue
+		}
+
+		var page *page.Page = new(page.Page)
+		page.Process(bs)
+		database = append(database, *page)
+		if page.Header.PageId != 0 {
+			fmt.Printf("Processed page %d\n", page.Header.PageId)
+		}
+
 	}
+	database.ShowStats()
 
 }
