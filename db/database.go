a1e4f77066237517f0ea94c4835e750ef4192e9e
Armen Arsakian <aarsakian@gmail.com>
2022-05-06 23:40:40 +0300

    moved code to database

---
+++ db/database.go
@@ -0,0 +1,16 @@
+package database
+
+import("MSSQLParser/page")
+
+
+type Database struct{
+	Pages page.Pages
+}
+
+func (db Database) ProcessPage(bs []byte) page.Page {
+	var page *page.Page = new(page.Page)
+	page.Process(bs)
+	
+	return *page
+}
+
