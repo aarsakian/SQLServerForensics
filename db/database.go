53daf4fd3bb920f6f14afb4e4102b420906a2b21
Armen Arsakian <aarsakian@gmail.com>
2022-05-10 22:45:29 +0300

    show pfs added

--- db/database.go
+++ db/database.go
@@ -1,16 +1,16 @@
 package database
 
-import("MSSQLParser/page")
+import (
+	"MSSQLParser/page"
+)
 
-
-type Database struct{
+type Database struct {
 	Pages page.Pages
 }
 
 func (db Database) ProcessPage(bs []byte) page.Page {
 	var page *page.Page = new(page.Page)
 	page.Process(bs)
-	
+
 	return *page
 }
-
