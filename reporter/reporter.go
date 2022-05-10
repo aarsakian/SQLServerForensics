aeeca834e19e1dd16a854e4770be403c796590f4
Armen Arsakian <aarsakian@gmail.com>
2022-05-10 22:43:54 +0300

    show pfs added

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -1,17 +1,17 @@
 package reporter
 
-import ("fmt"
-		db "MSSQLParser/db"
-		"MSSQLParser/page"
-		)
+import (
+	db "MSSQLParser/db"
+	"MSSQLParser/page"
+	"fmt"
+)
 
-
-
-type Reporter struct{
-	ShowGamExtents bool
+type Reporter struct {
+	ShowGamExtents  bool
 	ShowSGamExtents bool
-	ShowIAMExtents bool
-	ShowDataCols bool
+	ShowIAMExtents  bool
+	ShowDataCols    bool
+	ShowPFS         bool
 }
 
 func (rp Reporter) ShowStats(database db.Database) {
@@ -20,30 +20,28 @@ func (rp Reporter) ShowStats(database db.Database) {
 		if allocMap == nil {
 			continue
 		}
-		allocMap.ShowAllocations()
+
+		if rp.ShowPFS || rp.ShowIAMExtents ||
+			rp.ShowGamExtents || rp.ShowSGamExtents {
+			allocMap.ShowAllocations()
+		}
+
 	}
 
 }
 
-func (rp Reporter) PrintHeaderInfo(page page.Page){
+func (rp Reporter) PrintHeaderInfo(page page.Page) {
 	header := page.Header
-	fmt.Printf("Type %s Object is %d slots %d free space %d\n", page.GetType(), 
-	header.ObjectId, header.SlotCnt, header.FreeData)
+	fmt.Printf("Type %s Object is %d slots %d free space %d\n", page.GetType(),
+		header.ObjectId, header.SlotCnt, header.FreeData)
 }
 
-
-
-func (rp Reporter) PrintDataRowInfo(page page.Page){
+func (rp Reporter) PrintDataRowInfo(page page.Page) {
 	for slotId, dataRow := range page.DataRows {
 		fmt.Printf("Slot %d Record size offset %x \n", slotId, page.Slots[slotId])
 		if rp.ShowDataCols {
 			dataRow.ShowData()
 		}
-		
-	
-		
 
 	}
 }
-
-
