38f6459ac82aa415674c7de0910140abf85ad3bf
Armen Arsakian <aarsakian@gmail.com>
2022-05-10 22:44:59 +0300

    show pfs added

--- page/pfs.go
+++ page/pfs.go
@@ -1,27 +1,27 @@
 package page
 
-import ("fmt"
-		"MSSQLParser/utils")
+import (
+	"MSSQLParser/utils"
+	"fmt"
+)
 
 type PFSPage []PFS
 
 type PFS struct {
-	pageID uint8
+	pageID uint32
 	status string
 }
 
-func (pfsPage PFSPage) ShowAllocations(){
-	for _, pfs := range pfsPage{
-		fmt.Printf("page free space %d status %s \n", pfs.pageID, pfs.status)
+func (pfsPage PFSPage) ShowAllocations() {
+	for _, pfs := range pfsPage {
+		fmt.Printf("page %d status %s \n", pfs.pageID, pfs.status)
 	}
 }
-
 
 /*to be checked*/
 func (pfsPage PFSPage) FilterByAllocationStatus(status bool) AllocationMaps {
 	return PFSPage(utils.Filter(pfsPage, func(pfs PFS) bool {
-		return true 
+		return true
 	}))
 
 }
-
