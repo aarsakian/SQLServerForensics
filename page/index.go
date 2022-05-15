1da2bc32ca8619f0e376159d8d396b5812c9e00a
Armen Arsakian <aarsakian@gmail.com>
2022-05-15 20:27:51 +0300

    added new structures/enriching existings ones

---
+++ page/index.go
@@ -0,0 +1,10 @@
+package page
+
+type Index struct {
+	// min size is 6 bytes for an index record on a non-clustered index at leaf level,,
+	StatusA               uint8
+	NumberOfCols          uint16 //2
+	NullBitmap            uint16 //1-2
+	NumberOfVarLengthCols uint16 //0-
+	DataCols              *DataCols
+}
