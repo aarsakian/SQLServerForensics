46f03770984bb7ccd22ca6716e468b70cf82bba5
Armen Arsakian <aarsakian@gmail.com>
2022-08-31 13:50:38 +0300

    sys alloc units multiple entries per added new method to keep list per entry

--- db/database.go
+++ db/database.go
@@ -38,6 +38,23 @@ func (db Database) createMap(tablename string) map[any]any {
 	return results
 }
 
+func (db Database) createMapListGeneric(tablename string) map[any][]any {
+	results := map[any][]any{}
+	systemPages := db.Pages.FilterBySystemTables(tablename)
+	for _, tablePages := range systemPages {
+		for _, tablePage := range tablePages {
+			for _, datarow := range tablePage.DataRows {
+				objectId, val := datarow.SystemTable.GetData()
+
+				results[objectId] = append(results[objectId], val)
+
+			}
+		}
+
+	}
+	return results
+}
+
 func (db Database) createMapList(tablename string) map[int32][]page.Result[string, string, uint16, uint16] {
 	results := map[int32][]page.Result[string, string, uint16, uint16]{}
 	systemPages := db.Pages.FilterBySystemTables(tablename)
@@ -69,24 +86,25 @@ func (db Database) GetTablesInformation() []Table {
 	tablesMap := db.createMap("sysschobjs")
 	colsMap := db.createMapList("syscolpars")
 	tableAllocsMap := db.createMap("sysrowsets")
-	tableSysAllocsMap := db.createMap("sysallocationunits")
+	tableSysAllocsMap := db.createMapListGeneric("sysallocationunits")
 
 	var tables []Table
 	for tobjectId, tname := range tablesMap {
 		results, ok := colsMap[tobjectId.(int32)]
-
+		fmt.Printf("Processing table %s with object id %d\n", tname, tobjectId)
 		table := Table{Name: tname.(string), ObjectId: tobjectId.(int32)}
 
 		if ok {
 
 			columns := table.addColumns(results)
-			vid := 1 // keeps order var len cols
-			for _, c := range columns {
-				if c.isStatic() {
-					c.VarLenOrder = 0
+			vid := 0 // keeps order var len cols
+			//range copies values
+			for idx := range columns {
+				if columns[idx].isStatic() {
+					columns[idx].VarLenOrder = 0
 				} else {
 
-					c.VarLenOrder = uint16(vid)
+					columns[idx].VarLenOrder = uint16(vid)
 					vid++
 				}
 
@@ -98,11 +116,13 @@ func (db Database) GetTablesInformation() []Table {
 			table.PartitionId = partitionId.(uint64)
 		}
 
-		pageObjetId, ok := tableSysAllocsMap[table.PartitionId]
+		pageObjectIds, ok := tableSysAllocsMap[table.PartitionId]
 
 		if ok {
-			fmt.Printf("%d", pageObjetId)
-			table.setContent(db.Pages[pageObjetId.(uint32)])
+			for _, pageObjectId := range pageObjectIds {
+				table.setContent(db.Pages[pageObjectId.(uint32)])
+			}
+
 		}
 
 		tables = append(tables, table)
