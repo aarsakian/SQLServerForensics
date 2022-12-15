619480e1cbfbc3f5265a8f286af11f52e4d4617d
Armen Arsakian <aarsakian@gmail.com>
2022-12-15 22:36:06 +0200

    naming changes and condition to check page table and schema

--- db/tables.go
+++ db/tables.go
@@ -13,7 +13,7 @@ type Table struct {
 	Name         string
 	ObjectId     int32
 	rows         []ColMap
-	PartitionId  uint64
+	Rowsetid     uint64
 	PageObjectId int32
 	Schema       []Column
 	VarLenCols   []int
@@ -69,7 +69,7 @@ func (table *Table) addColumns(results []page.Result[string, string, uint16, uin
 func (table Table) printSchema() {
 	if table.Schema != nil {
 		fmt.Printf("\n table %s objectID %d  PartitionId %d page Object Id %d \n", table.Name,
-			table.ObjectId, table.PartitionId, table.PageObjectId)
+			table.ObjectId, table.Rowsetid, table.PageObjectId)
 		for _, col := range table.Schema {
 			fmt.Printf("Schema %s %s ", col.Name, col.Type)
 		}
@@ -99,12 +99,18 @@ func (table *Table) setContent(tablePages []page.Page) {
 		for _, datarow := range page.DataRows {
 			m := make(ColMap)
 			skippedVarCols := 0 // counts skipped var cols
+			nofCols := len(table.Schema)
+
+			if int(datarow.NumberOfCols) != nofCols { // mismatch data page and table schema!
+				continue
+			}
+
 			for _, col := range table.Schema {
 				len := reflect.ValueOf(datarow.NullBitmap).Len()
 				if len*8 < int(col.Order) {
 					fmt.Println("BOR")
 				}
-				if utils.HasFlagSet(datarow.NullBitmap, int(col.Order)) { //col is NULL skip when ASCII 49  (1)
+				if utils.HasFlagSet(datarow.NullBitmap, int(col.Order), nofCols) { //col is NULL skip when ASCII 49  (1)
 					skippedVarCols++
 					continue
 				}
