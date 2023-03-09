92bdea4150b5e9c31c94f5686b5b68a26e04c651
Armen Arsakian <aarsakian@gmail.com>
2023-03-09 20:24:32 +0200

    index row changes

--- page/index.go
+++ page/index.go
@@ -2,11 +2,14 @@ package page
 
 type IndexRow struct {
 	// min size is 6 bytes for an index record on a non-clustered index at leaf level,,
-	StatusA               uint8
-	NumberOfCols          uint16 //2
-	NullBitmap            uint16 //1-2
-	NumberOfVarLengthCols uint16 //0-
-	DataCols              *DataCols
+	StatusA               uint8   //1-2
+	FixedLenColsOffset    *uint16 //2-4  pminlen-1, does not exist infer by page header
+	FixedLenCols          []byte  //fixed portion of index
+	NumberOfCols          uint16  //4-6
+	NullBitmap            []byte  //6-7
+	NumberOfVarLengthCols uint16  //0-
+	VarLengthColOffsets   []int16
+	VarLenCols            *DataCols
 }
 
 func (indexRow IndexRow) ShowData() {
