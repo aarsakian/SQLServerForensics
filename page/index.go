9b58560ad8aef0e9969d51665cf2a7bcb919927c
Armen Arsakian <aarsakian@gmail.com>
2023-03-22 20:38:21 +0200

    index rows parsing and showing data added

--- page/index.go
+++ page/index.go
@@ -1,17 +1,41 @@
 package page
 
+import "fmt"
+
+type IndexRows []IndexRow
+
 type IndexRow struct {
-	// min size is 6 bytes for an index record on a non-clustered index at leaf level,,
-	StatusA               uint8   //1-2
-	FixedLenColsOffset    *uint16 //2-4  pminlen-1, does not exist infer by page header
-	FixedLenCols          []byte  //fixed portion of index
-	NumberOfCols          uint16  //4-6
-	NullBitmap            []byte  //6-7
-	NumberOfVarLengthCols uint16  //0-
+	// index record on a non-clustered index at leaf level,,
+	//Only if the index row has nullable columns are the field called NumberofCols and the null bitmap both present
+	StatusA               uint8  //1-2
+	FixedLenCols          []byte //0-
+	NumberOfCols          uint16 //
+	NullBitmap            []byte //
+	NumberOfVarLengthCols uint16 //0-
 	VarLengthColOffsets   []int16
 	VarLenCols            *DataCols
 }
 
-func (indexRow IndexRow) ShowData() {
+func (indexRow *IndexRow) ProcessVaryingCols(data []byte, offset int) {
+	baseOffset := int16(1 + len(indexRow.FixedLenCols) + 2 + len(indexRow.NullBitmap) + 2 + len(indexRow.VarLengthColOffsets)*2) //status + len of fixed cols
+	var datacols DataCols
 
+	for idx, varLenColOffset := range indexRow.VarLengthColOffsets {
+
+		dst := make([]byte, varLenColOffset-baseOffset) //buffer for varying le cols
+		copy(dst, data[baseOffset:varLenColOffset])
+		datacols = append(datacols,
+			DataCol{id: idx, content: dst, offset: uint16(varLenColOffset)})
+		baseOffset = varLenColOffset
+	}
+	indexRow.VarLenCols = &datacols
+}
+
+func (indexRow IndexRow) ShowData() {
+	fmt.Printf("Fixed Cols %x ", indexRow.FixedLenCols)
+	for _, datacol := range *indexRow.VarLenCols {
+		fmt.Printf(" Varying cols %x ", datacol.content)
+
+	}
+	fmt.Printf("\n")
 }
