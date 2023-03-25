32bd5b252f376d9bddc7205df2bc1d6d7288ad85
Armen Arsakian <aarsakian@gmail.com>
2023-03-25 14:25:22 +0200

    moved cols related code to columns/changes in col add content arguments

--- db/tables.go
+++ db/tables.go
@@ -8,8 +8,6 @@ import (
 	"sort"
 )
 
-type ColMap map[string][]byte
-
 type Table struct {
 	Name              string
 	ObjectId          int32
@@ -23,143 +21,12 @@ type Table struct {
 	indexType         string
 }
 
-type Column struct {
-	Name        string
-	Type        string
-	Size        int16
-	Order       uint16
-	VarLenOrder uint16
-	CollationId uint32
-	Precision   uint8
-	Scale       uint8
-}
-
-type SqlVariant struct {
-	BaseType   uint8
-	Version    uint8
-	Properties *SqlVariantProperties
-	Value      []byte
-}
-
-type SqlVariantProperties struct {
-	Precision     uint8
-	Scale         uint8
-	MaximumLength uint16
-	CollationId   uint32
-}
-
-func (c Column) parseDecimal(data []byte) string {
-	return utils.DecimalToStr(data, c.Precision, c.Scale)
-
-}
-
-func (sqlVariant SqlVariant) getData() string {
-	if sqlVariant.BaseType == 0x23 {
-		return fmt.Sprintf("%d", utils.ToInt32(sqlVariant.Value))
-	} else if sqlVariant.BaseType == 0x7f {
-		return fmt.Sprintf("%d", utils.ToInt64(sqlVariant.Value))
-	} else if sqlVariant.BaseType == 0xad { //string
-		return fmt.Sprintf("%x", sqlVariant.Value)
-	}
-	return ""
-}
-
-func (c Column) isStatic() bool {
-
-	if c.Type == "varchar" || c.Type == "nvarchar" ||
-		c.Type == "varbinary" || c.Type == "xml" || c.Type == "text" ||
-		c.Type == "ntext" || c.Type == "image" || c.Type == "hierarchyid" ||
-		c.Type == "float" || c.Type == "sql_variant" || c.Type == "sysname" {
-		return false
-	} else {
-		return true
-	}
-
-}
-
-func (c Column) parseSqlVariant(data []byte) SqlVariant {
-	var sqlVariant *SqlVariant = new(SqlVariant)
-	utils.Unmarshal(data, sqlVariant)
-	var sqlVariantProperties SqlVariantProperties
-	if sqlVariant.BaseType == 0x38 { //int
-		sqlVariantProperties = SqlVariantProperties{Precision: data[2], Scale: data[3]}
-		sqlVariant.Value = data[3:]
-	} else if sqlVariant.BaseType == 0x23 { //string
-
-		sqlVariantProperties = SqlVariantProperties{MaximumLength: utils.ToUint16(data[2:4]),
-			CollationId: utils.ToUint32(data[4:8])}
-		sqlVariant.Value = data[8:]
-	}
-	sqlVariant.Properties = &sqlVariantProperties
-	return *sqlVariant
-}
-
-func (c Column) toString(data []byte) string {
-	if len(data) == 0 {
-		mslogger.Mslogger.Warning(fmt.Sprintf("Empty data col %s", c.Name))
-		return ""
-	}
-	if c.Type == "varchar" || c.Type == "nvarchar" || c.Type == "text" || c.Type == "ntext" {
-		if c.CollationId == 872468488 { //SQL_Latin1_General_CP1_CI_AS
-			return string(data)
-		} else {
-			return utils.DecodeUTF16(data)
-		}
-
-	} else if c.Type == "datetime2" {
-		return utils.DateTime2Tostr(data)
-	} else if c.Type == "datetime" {
-		return utils.DateTimeTostr(data)
-	} else if c.Type == "int" {
-		return fmt.Sprintf("%d", utils.ToInt32(data))
-	} else if c.Type == "smallint" {
-		return fmt.Sprintf("%d", utils.ToInt16(data))
-	} else if c.Type == "tinyint" {
-		return fmt.Sprintf("%d", utils.ToInt8(data))
-	} else if c.Type == "bigint" {
-		return fmt.Sprintf("%d", utils.ToInt64(data))
-	} else if c.Type == "varbinary" {
-		return fmt.Sprintf("%x", data)
-	} else if c.Type == "decimal" {
-
-		return c.parseDecimal(data)
-	} else if c.Type == "sql_variant" {
-		sqlVariant := c.parseSqlVariant(data)
-		return sqlVariant.getData()
-	} else if c.Type == "bit" {
-		return fmt.Sprintf("%1d", utils.ToInt8(data))
-	} else if c.Type == "uniqueidentifier" {
-		return fmt.Sprintf("%x-%x-%x-%x-%x", utils.Reverse(data[0:4]), utils.Reverse(data[4:6]),
-			utils.Reverse(data[6:8]), data[8:10], data[10:16])
-	} else {
-		mslogger.Mslogger.Warning(fmt.Sprintf("col %s type %s not yet implemented", c.Name, c.Type))
-		return fmt.Sprintf("unhandled type %s %x", c.Type, data[:])
-	}
-}
-
-func (c *Column) addContent(datarow page.DataRow,
-	lobPages page.PageMapIds, textLOBPages page.PageMapIds, fixColsOffset int) []byte {
-	if datarow.SystemTable != nil {
-		return utils.FindValueInStruct(c.Name, datarow.SystemTable)
-	} else {
-		return datarow.ProcessData(c.Order, c.Size, c.isStatic(),
-			c.VarLenOrder, lobPages, textLOBPages, fixColsOffset)
-	}
-
-}
-
 func (table Table) getHeader() utils.Record {
 	var record utils.Record
 	for _, c := range table.Schema {
 		record = append(record, c.Name)
 	}
 	return record
-}
-
-func (c Column) Print(data []byte) {
-
-	fmt.Printf("%s ", c.toString(data))
-
 }
 
 func (table *Table) addColumn(name string, coltype string, size int16, order uint16, collationId uint32, prec uint8, scale uint8) {
@@ -322,6 +189,10 @@ func (table Table) printData(showtorow int, showrow int) {
 
 }
 
+func (table *Table) updateColOffsets(cid uint32, offset int32) {
+	table.Schema[cid].Offset = offset
+}
+
 func (table *Table) setContent(dataPages page.PageMapIds,
 	lobPages page.PageMapIds, textLobPages page.PageMapIds) {
 	forwardPages := map[uint32][]uint32{} //list by when seen forward pointer with parent page
@@ -352,7 +223,7 @@ func (table *Table) setContent(dataPages page.PageMapIds,
 				mslogger.Mslogger.Warning(msg)
 				continue
 			}
-			fixColsOffset := 0
+
 			for colnum, col := range table.Schema {
 				//schema is sorted by colorder use colnum instead of col.Order
 				if colnum+1 != int(col.Order) {
@@ -366,13 +237,7 @@ func (table *Table) setContent(dataPages page.PageMapIds,
 				}
 
 				//mslogger.Mslogger.Info(col.Name + " " + fmt.Sprintf("%s %d %s %d", col.isStatic(), col.Order, col.Type, col.Size))
-				m[col.Name] = col.addContent(datarow, lobPages, textLobPages, fixColsOffset)
-
-				if col.isStatic() {
-
-					fixColsOffset += int(col.Size)
-
-				}
+				m[col.Name] = col.addContent(datarow, lobPages, textLobPages)
 
 			}
 			rows = append(rows, m)
