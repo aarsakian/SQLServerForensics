a370106c810bc45e72158b8a682f88292fa0ea17
aarsakian <aarsakian@gmail.com>
2023-12-11 05:30:06 -0800

    using better api for path join

--- exporter/tocsv.go
+++ exporter/tocsv.go
@@ -7,12 +7,12 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path"
+	"path/filepath"
 )
 
 func writeCSV(records utils.Records, filename string, folder string) {
 
-	file, err := os.Create(fmt.Sprintf("%s.csv", path.Join(folder, filename)))
+	file, err := os.Create(fmt.Sprintf("%s.csv", filepath.Join(folder, filename)))
 	defer file.Close()
 	if err != nil {
 		mslogger.Mslogger.Error(fmt.Sprintf("failed to open file %s", err))
