bf60f1d27e3cf1ca5558adb0880539bf84a2f9aa
Armen Arsakian <aarsakian@gmail.com>
2024-04-22 18:17:37 +0300

    rewrote exporting tables using go channels

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -2,12 +2,11 @@ package exporter
 
 import (
 	db "MSSQLParser/db"
-	mslogger "MSSQLParser/logger"
 	"MSSQLParser/utils"
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 type Writer interface {
@@ -21,10 +20,10 @@ type Exporter struct {
 }
 
 func (exp Exporter) Export(database db.Database, tablename string, tabletype string, selectedTableRow int) {
-	var records utils.Records
+
 	var images utils.Images
 
-	err := os.MkdirAll(exp.Path, 0750)
+	err := os.MkdirAll(filepath.Join(exp.Path, database.Fname), 0750)
 
 	if err != nil && !os.IsExist(err) {
 		log.Fatal(err)
@@ -48,7 +47,11 @@ func (exp Exporter) Export(database db.Database, tablename string, tabletype str
 			}
 		}
 
-		records = table.GetRecords(selectedTableRow)
+		wg := new(sync.WaitGroup)
+		wg.Add(2)
+		records := make(chan utils.Record, 100)
+
+		go table.GetRecords(wg, selectedTableRow, records)
 
 		if exp.Image {
 			images = table.GetImages()
@@ -58,9 +61,9 @@ func (exp Exporter) Export(database db.Database, tablename string, tabletype str
 		}
 
 		if exp.Format == "csv" {
-			msg := fmt.Sprintf("Exporting Table %s with %d records", table.Name, len(records)-1)
-			mslogger.Mslogger.Info(msg)
-			writeCSV(records, table.Name, filepath.Join(exp.Path, table.Type))
+
+			go writeCSV(wg, records, table.Name, filepath.Join(exp.Path, table.Type))
+			wg.Wait()
 		}
 
 	}
