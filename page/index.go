68e770d5fc0a05882cb2ab0f063e560082264373
Armen Arsakian <arsakian@digitalforensics.gr>
2024-09-05 11:59:29 +0300

    index type changes/bugfixes in keyvalue

--- page/index.go
+++ page/index.go
@@ -1,6 +1,7 @@
 package page
 
 import (
+	mslogger "MSSQLParser/logger"
 	"MSSQLParser/utils"
 	"fmt"
 	"unsafe"
@@ -8,7 +9,8 @@ import (
 
 // If a clustered index is created, must be unique so that nonclustered index entries can point to exactly one specific row.
 // if the index isn't defined as unique, the duplicated key values include a uniquifier  a 4-byte integer to make each nonunique key value unique.
-// , all nonclustered index entries must refer to exactly one row. Because that pointer is the clustering key in SQL Server, the clustering key must be unique
+// , all nonclustered index entries must refer to exactly one row. Because that pointer is the clustering key in SQL Server,
+//the clustering key must be unique
 // clustered index root -> intermediate level rowid( child fileId+child PageId) +key value and so on
 // The leaf level of a clustered index is the data itself, the data is copied and ordered based on the clustering key r
 // Each row in the non-leaf levels has one entry for every page of the level below,  this entry includes an index key value and a 6-byte pointer to reference the page
@@ -24,8 +26,8 @@ import (
 
 type IndexRows []IndexRow
 
-type IndexIntermediateClustered struct {
-	PageID      uint32
+type IndexNoNLeafClustered struct {
+	ChildPageID uint32
 	FileID      uint16
 	ChildFileID uint16 //? not sure
 	RowSize     uint16
@@ -38,6 +40,10 @@ type IndexNoNLeaf struct {
 	ChildFileID uint16
 }
 
+type IndexLeafClustered struct {
+	KeyValue []byte
+}
+
 type IndexLeafNoNClustered struct {
 	KeyValue    []byte //0-?
 	ChildPageID uint32
@@ -48,10 +54,11 @@ type IndexLeafNoNClustered struct {
 type IndexRow struct {
 	// index record on a non-clustered index at leaf level,,
 	//Only if the index row has nullable columns are the field called NumberofCols and the null bitmap both present
-	StatusA               uint8 //0-1
-	NoNLeaf               *IndexNoNLeaf
-	IntermediateClustered *IndexIntermediateClustered
-	LeafNoNClustered      *IndexLeafNoNClustered
+	StatusA          uint8 //0-1
+	NoNLeaf          *IndexNoNLeaf
+	NoNLeafClustered *IndexNoNLeafClustered
+	LeafNoNClustered *IndexLeafNoNClustered
+	LeafClustered    *IndexLeafClustered
 
 	FixedLenCols          []byte //0-
 	NumberOfCols          uint16 //
@@ -61,19 +68,19 @@ type IndexRow struct {
 	VarLenCols            *DataCols
 }
 
-func (indexRow *IndexRow) Parse(data []byte) {
+func (indexRow *IndexRow) Parse(data []byte, offset int) {
 	indexRow.StatusA = data[0]
-	if indexRow.IsIntermediateClusteredRecord() {
-		indexIntermediate := new(IndexIntermediateClustered)
+	if indexRow.IsNonLeafRecord() {
+		indexIntermediate := new(IndexNoNLeafClustered)
 		utils.Unmarshal(data[1:], indexIntermediate)
 
 		structSize := int(unsafe.Sizeof(indexIntermediate))
 		indexIntermediate.KeyValue = make([]byte, len(data[1+structSize:]))
 		copy(indexIntermediate.KeyValue, data[1+structSize:])
 
-		indexRow.IntermediateClustered = indexIntermediate
+		indexRow.NoNLeafClustered = indexIntermediate
 
-	} else if indexRow.IsRootRecordClustered() && len(data) > 6 { // root ?
+	} else if indexRow.IsRecordClustered() && len(data) > 6 { // root ?
 
 		indexNoNLeaf := new(IndexNoNLeaf)
 
@@ -85,6 +92,13 @@ func (indexRow *IndexRow) Parse(data []byte) {
 			copy(indexNoNLeaf.KeyValue, data[1:len(data)-structSize])
 			indexRow.NoNLeaf = indexNoNLeaf
 		}
+
+	} else if indexRow.IsRecordClustered() { //leaf
+		indexLeafClustered := new(IndexLeafClustered)
+
+		indexLeafClustered.KeyValue = make([]byte, len(data))
+		copy(indexLeafClustered.KeyValue, data)
+		indexRow.LeafClustered = indexLeafClustered
 
 	} else if indexRow.IsLeafNoNClusteredRecord() && len(data) == 6 {
 		indexNoNLeaf := new(IndexNoNLeaf)
@@ -101,6 +115,8 @@ func (indexRow *IndexRow) Parse(data []byte) {
 		copy(indexLeafNoNClustered.KeyValue, data[1:len(data)-structSize])
 
 		indexRow.LeafNoNClustered = indexLeafNoNClustered
+	} else {
+		mslogger.Mslogger.Warning(fmt.Sprintf("Index row has NoNLeaf %d", offset))
 	}
 
 }
@@ -128,11 +144,11 @@ func (indexRow IndexRow) IsIndexRecord() bool {
 	return indexRow.StatusA&3 == 3
 }
 
-func (indexRow IndexRow) IsIntermediateClusteredRecord() bool {
+func (indexRow IndexRow) IsNonLeafRecord() bool {
 	return indexRow.StatusA&38 == 38
 }
 
-func (indexRow IndexRow) IsRootRecordClustered() bool {
+func (indexRow IndexRow) IsRecordClustered() bool {
 	return indexRow.StatusA&6 == 6
 }
 
@@ -145,11 +161,11 @@ func (indexRow IndexRow) IsGhostRecord() bool {
 }
 
 func (indexRow IndexRow) ShowData() {
-	if indexRow.IntermediateClustered != nil {
-		fmt.Printf("Intermediate CLustered Child FileID %d PageID %d  Key %x RowSize %d\n",
-			indexRow.IntermediateClustered.ChildFileID,
-			indexRow.IntermediateClustered.PageID, indexRow.IntermediateClustered.KeyValue,
-			indexRow.IntermediateClustered.RowSize)
+	if indexRow.NoNLeafClustered != nil {
+		fmt.Printf("Non Leaf CLustered Child FileID %d PageID %d  Key %x RowSize %d\n",
+			indexRow.NoNLeafClustered.ChildFileID,
+			indexRow.NoNLeafClustered.ChildPageID, indexRow.NoNLeafClustered.KeyValue,
+			indexRow.NoNLeafClustered.RowSize)
 	} else if indexRow.LeafNoNClustered != nil {
 		fmt.Printf("Leaf Non Clustered ChildID %d Key %x RowID %d\n",
 			indexRow.LeafNoNClustered.ChildPageID,
