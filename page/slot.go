d6f5756147a9252516a8125edcae0b421c70681e
Armen Arsakian <aarsakian@gmail.com>
2025-02-26 23:28:00 +0200

    introduced slots sorted by order

--- page/slot.go
+++ page/slot.go
@@ -3,42 +3,60 @@ package page
 import (
 	"MSSQLParser/utils"
 	"encoding/binary"
-	"sort"
 )
 
 type Slot struct {
-	Order  int
-	Offset uint16
+	Order                int
+	Offset               uint16
+	Deleted              bool
+	ActualDataRowSize    uint16
+	AllocatedDataRowSize uint16
 }
 
 type Slots []Slot
-type SortedSlots []Slot
+type SortedSlotsByOffset []Slot
 
-func (s SortedSlots) Len() int {
+func (s SortedSlotsByOffset) Len() int {
 	return len(s)
 
 }
 
-func (s SortedSlots) Less(i, j int) bool {
+func (s SortedSlotsByOffset) Less(i, j int) bool {
 	return int(s[i].Offset) < int(s[j].Offset)
 }
 
-func (s SortedSlots) Swap(i, j int) {
+func (s SortedSlotsByOffset) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
-func retrieveSortedSlots(data []byte) Slots {
-	var slots Slots
+type SortedSlotsByOrder []Slot
+
+func (s SortedSlotsByOrder) Len() int {
+	return len(s)
+
+}
+
+func (s SortedSlotsByOrder) Less(i, j int) bool {
+	return int(s[i].Order) < int(s[j].Order)
+}
+
+func (s SortedSlotsByOrder) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func retrieveSlots(data []byte) Slots {
+	slots := make(Slots, len(data)/2)
+
+	pos := 0
 
 	for idx := 0; idx < binary.Size(data); idx += 2 {
-		slot := Slot{Offset: utils.ToUint16(data[idx : idx+2])}
-		slots = append(slots, slot)
+		slots[pos] = Slot{Order: len(slots) - pos - 1,
+			Offset:  utils.ToUint16(data[idx : idx+2]),
+			Deleted: false,
+		}
+		pos++
 
 	}
 
-	for idx := range slots {
-		slots[idx].Order = len(slots) - idx - 1
-	}
-	sort.Sort(SortedSlots(slots))
 	return slots
 }
