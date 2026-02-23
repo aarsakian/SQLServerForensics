b9ee506ee0916644044c031ef9cfbfe1ca48fe43
Armen Arsakian <aarsakian@gmail.com>
2026-02-23 22:17:19 +0200

    corrected fileheader structure/ added slice case and showinfo method

--- page/fileheader.go
+++ page/fileheader.go
@@ -5,6 +5,7 @@ import (
 	"MSSQLParser/utils"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"reflect"
 )
 
@@ -12,61 +13,43 @@ type FileHeader struct {
 
 	// ---- File header record starts at offset 0x0060 ----
 
-	BindingID                   [16]byte // 0x008a 138
-	FileIDProp                  uint16   // 154
-	FileID                      uint16   //
-	Size                        uint32
-	MaxSize                     uint32
-	Growth                      uint32
-	Perf                        uint32
-	BackupLSN                   utils.LSN
-	FirstUpdateLSN              utils.LSN
-	OldestRestoreLSN            utils.LSN
-	FirstNonloggedUpdateLsn     utils.LSN
-	MinSize                     uint32
-	Status                      uint32
-	UserShrinkSize              uint32 //d4
-	SectorSize                  uint32
-	MaxLSN                      utils.LSN
-	FirstLSN                    utils.LSN
-	CreateLSN                   utils.LSN
-	DifferentialBaseLsn         utils.LSN
-	DifferentialBaseGuid        [16]byte
-	FileOfflineLsn              utils.LSN
-	FileIdGuid                  [16]byte
-	RestoreStatus               uint32
-	RestoreRedoStartLsn         utils.LSN
-	RestoreSourceGuid           [16]byte
-	MaxLsnBranchId              [16]byte
-	ReadOnlyLsn                 utils.LSN
-	ReadWriteLsn                utils.LSN
-	RestoreDifferentialBaseLsn  utils.LSN
-	RestoreDifferentialBaseGuid [16]byte
-
-	/*FileGroupID uint32 //
-
-	LogGroupGUID [16]byte // 0x0078 (rarely used)
-	FileGUID     [16]byte // 0x0088
-	DiffBaseGUID [16]byte // 0x0098
-
-	Unknown              [4]byte
-	ResotoreRedoStartLSN utils.LSN
-
-	UknownLSN   utils.LSN
-	DiffBaseLSN uint64 // 0x00A8
-
-	// FileName is stored as Unicode (NVARCHAR), max 260 chars.
-	// You’ll typically decode this manually from UTF-16LE.
-	FileNameRaw [260 * 2]byte // 0x00B8
-
-	// Optional secondary name / mirror; version-dependent.
-	FileName2Raw [260 * 2]byte // 0x02B0
-
-	TDEFlag       uint32   // 0x04AC
-	TDEThumbprint [16]byte // 0x04B0
-
-	DropLSN    uint64 // 0x04C8
-	BackupLSN2 uint64 // 0x04D0*/
+	BindingID                    [16]byte // 0x008a 138
+	FileIDProp                   uint16   // 154
+	FileID                       uint16   //
+	Size                         uint32
+	MaxSize                      uint32
+	Growth                       uint32
+	Perf                         uint32
+	BackupLSN                    utils.LSN
+	FirstUpdateLSN               utils.LSN
+	OldestRestoreLSN             utils.LSN
+	FirstNonloggedUpdateLsn      utils.LSN
+	MinSize                      uint32
+	Status                       uint32
+	UserShrinkSize               uint32 //d4
+	SectorSize                   uint32
+	MaxLSN                       utils.LSN
+	FirstLSN                     utils.LSN
+	CreateLSN                    utils.LSN
+	DifferentialBaseLsn          utils.LSN
+	DifferentialBaseGuid         [16]byte
+	FileOfflineLsn               utils.LSN
+	FileIdGuid                   [16]byte
+	RestoreStatus                uint32
+	RestoreRedoStartLsn          utils.LSN
+	FileName                     []byte //database Name
+	RestoreSourceGuid            [16]byte
+	MaxLsnBranchId               [16]byte
+	SecondaryRedoStartLsn        utils.LSN
+	SecondaryDifferentialBaseLsn utils.LSN
+	ReadOnlyLsn                  utils.LSN
+	ReadWriteLsn                 utils.LSN
+	RestoreDifferentialBaseLsn   utils.LSN
+	RestoreDifferentialBaseGuid  [16]byte
+	/*RestorePathOrigin = [NULL]          DatabaseEncryptionFileState = [NULL]
+	FCBFileDEK = [NULL]                 ProxyFileIdProp = [Error accessing Column]
+	ForeignRedoLsn = [NULL]             ForeignRedoTime = [NULL]            ForeignRedoOldestBeginLsn = [NULL]
+	*/
 
 }
 
@@ -103,14 +86,28 @@ func (fileHeader *FileHeader) Parse(datarow datac.DataRow) error {
 			dst := arr.Slice(0, n).Bytes()
 			copy(dst, val)
 			field.Set(arr)
+		case reflect.Slice:
+
+			var dst []byte
+
+			dst = make([]byte, len(val))
+			copy(dst, val)
+
+			field.Set(reflect.ValueOf(dst))
 
 		}
+
 	}
 	return nil
 }
 
 func (fileHeader FileHeader) GetFileName() string {
-	return ""
-	//return utils.CleanUTF16LE(utils.DecodeUTF16(fileHeader.FileNameRaw[:]))
 
+	return utils.CleanUTF16LE(utils.DecodeUTF16(fileHeader.FileName[:]))
+
+}
+
+func (fileHeader FileHeader) ShowInfo() {
+	fmt.Printf("NofPages %d FileName %s MaxLsnBranchId %s\n", fileHeader.Size, fileHeader.GetFileName(),
+		utils.StringifyGUID(fileHeader.MaxLsnBranchId[:]))
 }
