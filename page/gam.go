08c0c9ea27384683ece964527de0006714b520a0
Armen Arsakian <aarsakian@gmail.com>
2026-02-24 19:03:00 +0200

    map retrieved per pageid/ better naming

--- page/gam.go
+++ page/gam.go
@@ -4,13 +4,12 @@ import (
 	"MSSQLParser/utils"
 	"fmt"
 	"reflect"
-	"strings"
 )
 
 type GAMExtents []GAMExtent
 
 type GAMExtent struct {
-	extent    int
+	pageid    int
 	allocated bool
 }
 
@@ -24,10 +23,10 @@ func (gamExtents GAMExtents) ShowAllocations() {
 	for _, gamextent := range gamExtents[1:] {
 		if gamextent.allocated != prevGamExtent.allocated {
 
-			fmt.Printf("(%d:%d) = %s \n", startPageId*8, prevGamExtent.extent*8,
+			fmt.Printf("(%d:%d) = %s \n", startPageId*8, prevGamExtent.pageid*8,
 				(map[bool]string{true: "ALLOCATED", false: "NOT ALLOCATED"})[prevGamExtent.allocated])
 
-			startPageId = gamextent.extent
+			startPageId = gamextent.pageid
 		}
 
 		prevGamExtent = gamextent
@@ -51,15 +50,17 @@ func (gamExtents GAMExtents) GetStats() (int, int) {
 }
 
 func (gamExtents GAMExtents) GetAllocationStatus(pageId uint32) string {
-	var status strings.Builder
 
 	for _, gam := range gamExtents {
-		if pageId < uint32(gam.extent*8) || pageId > uint32(gam.extent*8+8) {
-			status.WriteString(fmt.Sprintf("%d NOT ALLOCATED\n", pageId))
-		} else {
-			status.WriteString(fmt.Sprintf("%d ALLOCATED\n", pageId))
+		if pageId == uint32(gam.pageid) {
+			if gam.allocated { //
+				return fmt.Sprintf("%d GAM ALLOCATED\n", pageId)
+			} else {
+				return fmt.Sprintf("%d GAM ΝΟΤ ALLOCATED\n", pageId)
+			}
+
 		}
 	}
 
-	return status.String()
+	return ""
 }
