1d2b77dee228c121d204522904cd7338cfad17a4
Armen Arsakian <aarsakian@gmail.com>
2026-03-13 23:35:12 +0200

    functions for html schema creation and main html page

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"text/template"
 )
 
 type Writer interface {
@@ -36,6 +37,9 @@ func (exp Exporter) Export(expWg *sync.WaitGroup, selectedTableRow []int, colnam
 	databaseName string, databaseFolder string, tables <-chan db.Table) {
 	defer expWg.Done()
 
+	var tmpl *template.Template
+	var indexFile *os.File
+
 	databaseName = filepath.Base(databaseName)
 	err := os.RemoveAll(filepath.Join(exp.Path, databaseFolder, databaseName))
 	if err != nil {
@@ -44,6 +48,24 @@ func (exp Exporter) Export(expWg *sync.WaitGroup, selectedTableRow []int, colnam
 	err = os.MkdirAll(filepath.Join(exp.Path, databaseFolder, databaseName), 0750)
 	if err != nil && !os.IsExist(err) {
 		log.Fatal(err)
+	}
+
+	if exp.Format == "html" {
+		indexPath := filepath.Join(exp.Path, databaseFolder, databaseName, "index.html")
+		indexFile, err = os.Create(indexPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer indexFile.Close()
+
+		tmpl, err = template.ParseFiles("templates/index.tmpl")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := tmpl.ExecuteTemplate(indexFile, "header", nil); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	for table := range tables {
@@ -70,9 +92,18 @@ func (exp Exporter) Export(expWg *sync.WaitGroup, selectedTableRow []int, colnam
 			go WriteCSV(wg, records, table.Name, expPath, headers)
 			wg.Wait()
 		case "html":
+
+			writeIndex(tmpl, indexFile, table)
+			writeSchema(table.Schema, expPath, table.Name)
 			wg.Add(1)
 			go WriteHTML(wg, records, table.Name, expPath, headers)
 			wg.Wait()
+		}
+	}
+
+	if exp.Format == "html" {
+		if err := tmpl.ExecuteTemplate(indexFile, "footer", nil); err != nil {
+			log.Fatal(err)
 		}
 	}
 
