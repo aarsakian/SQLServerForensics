d63464cb00011039a6f2f3df4124f211741b0991
Armen Arsakian <aarsakian@gmail.com>
2026-03-13 23:34:35 +0200

    functions for html schema creation and main html page

--- exporter/tohtml.go
+++ exporter/tohtml.go
@@ -1,6 +1,7 @@
 package exporter
 
 import (
+	"MSSQLParser/db"
 	mslogger "MSSQLParser/logger"
 	"MSSQLParser/utils"
 	"fmt"
@@ -44,4 +45,53 @@ func WriteHTML(wg *sync.WaitGroup, records <-chan utils.Record, filename string,
 		}
 	}
 
+}
+
+func writeSchema(schema []db.Column, folder string, filename string) {
+	fpath := filepath.Join(folder, fmt.Sprintf("%s_schema.html", filename))
+	file, err := os.Create(fpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	tmpl, err := template.ParseFiles("templates/schema.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := struct {
+		TableName string
+
+		Columns []db.Column
+	}{
+		TableName: filename,
+
+		Columns: schema,
+	}
+
+	if err := tmpl.ExecuteTemplate(file, "schema", data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func writeIndex(tmpl *template.Template, indexFile *os.File, table db.Table) {
+	htmlPath := filepath.ToSlash(filepath.Join(table.Type, table.Name+".html"))
+	schemaPath := filepath.ToSlash(filepath.Join(table.Type, table.Name+"_schema.html"))
+	rowData := struct {
+		Name           string
+		Type           string
+		PageIDsPerType map[string][]uint32
+		HTMLPath       string
+		SchemaPath     string
+	}{
+		Name:           table.Name,
+		Type:           table.Type,
+		PageIDsPerType: table.PageIDsPerType,
+		HTMLPath:       htmlPath,
+		SchemaPath:     schemaPath,
+	}
+	if err := tmpl.ExecuteTemplate(indexFile, "row", rowData); err != nil {
+		log.Fatal(err)
+	}
 }
