8e5260783a887882be0213973b4b6b57e770a6e9
Armen Arsakian <aarsakian@gmail.com>
2026-03-16 13:18:08 +0200

    pagination functionality  added

--- exporter/tohtml.go
+++ exporter/tohtml.go
@@ -16,40 +16,101 @@ import (
 type HTMLExporter struct {
 }
 
+type tableTemplateData struct {
+	Headers     []string
+	PrevPage    string
+	NextPage    string
+	CurPage     string
+	Name        string
+	IsPaginated bool
+}
+
+func paginatedPageName(filename string, page int) string {
+	return fmt.Sprintf("%s_%d.html", filename, page)
+}
+
 func WriteHTML(wg *sync.WaitGroup, records <-chan utils.Record, filename string,
 	folder string, headers []string) {
 	defer wg.Done()
 
-	fpath := filepath.Join(folder, fmt.Sprintf("%s.html", filename))
-	file, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	var paginatedFile *os.File
+	var paginatedfpath string
 
-	if err != nil {
-		mslogger.Mslogger.Error(fmt.Sprintf("failed to open file %s", err))
-		fmt.Printf("%s\n", err)
-	}
-	defer file.Close()
+	msg := fmt.Sprintf("Exporting data from %s", filename)
+	fmt.Printf(msg + " \n")
+	mslogger.Mslogger.Info(msg)
 
 	tmpl, err := template.ParseFiles("templates/table.tmpl")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	msg := fmt.Sprintf("Exporting data from %s", filename)
-	fmt.Printf(msg + " \n")
-	mslogger.Mslogger.Info(msg)
+	data := tableTemplateData{Headers: headers,
+		Name: filename, IsPaginated: false}
 
-	data := struct{ Headers []string }{Headers: headers}
-	if err := tmpl.ExecuteTemplate(file, "header", data); err != nil {
+	fpath := filepath.Join(folder, fmt.Sprintf("%s.html", filename))
+	file, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	if err = tmpl.ExecuteTemplate(file, "header", data); err != nil {
 		log.Fatal(err)
 	}
 
+	nofRows := 0
+	RowsPerPage := 1000
+
+	data.IsPaginated = false
+
 	for record := range records {
+		// Render paginated HTML header
+		if nofRows%RowsPerPage == 0 {
+			data.IsPaginated = true
+			paginatedfpath = filepath.Join(folder, fmt.Sprintf("%s_%d.html", filename, nofRows/RowsPerPage))
+			paginatedFile, err = os.OpenFile(paginatedfpath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+
+			data.NextPage = paginatedPageName(filename, nofRows/RowsPerPage+1)
+			data.CurPage = fmt.Sprintf("%d", nofRows/RowsPerPage+1)
+
+			if nofRows/RowsPerPage-1 < 0 {
+				data.PrevPage = ""
+			} else {
+				data.PrevPage = paginatedPageName(filename, nofRows/RowsPerPage-1)
+			}
+
+			if err = tmpl.ExecuteTemplate(paginatedFile, "header", data); err != nil {
+				log.Fatal(err)
+			}
+			defer paginatedFile.Close()
+		}
+
+		// Render paginated HTML row
+		data.IsPaginated = true
+		if err := tmpl.ExecuteTemplate(paginatedFile, "row", record.Vals); err != nil {
+			log.Fatal(err)
+		}
+
+		// Render full HTML row
+		data.IsPaginated = false
 		if err := tmpl.ExecuteTemplate(file, "row", record.Vals); err != nil {
 			log.Fatal(err)
 		}
+		nofRows++
+
+		// Render paginated HTML footer
+		if nofRows%RowsPerPage == 0 {
+			data.IsPaginated = true
+			if err := tmpl.ExecuteTemplate(paginatedFile, "footer", data); err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 
-	if err := tmpl.ExecuteTemplate(file, "footer", nil); err != nil {
+	// Render full HTML footer
+	data.IsPaginated = false
+	if err := tmpl.ExecuteTemplate(file, "footer", data); err != nil {
 		log.Fatal(err)
 	}
 
@@ -83,12 +144,14 @@ func writeSchema(schema []tables.Column, folder string, filename string) {
 	}
 }
 
-func writeIndex(tmpl *template.Template, indexFile *os.File, table db.Table) {
+func writeTOC(tmpl *template.Template, indexFile *os.File, table db.Table) {
 
 	exportPaths := make([]string, 0, len(table.Indexes)+2)
 
 	exportPaths = append(exportPaths, filepath.ToSlash(filepath.Join(table.Type, table.Name+".html")))
+	exportPaths = append(exportPaths, filepath.ToSlash(filepath.Join(table.Type, fmt.Sprintf("%s_0.html", table.Name))))
 	exportPaths = append(exportPaths, filepath.ToSlash(filepath.Join(table.Type, table.Name+"_schema.html")))
+
 	for _, index := range table.Indexes {
 		exportPaths = append(exportPaths, filepath.ToSlash(filepath.Join(table.Type, fmt.Sprintf("%s_index.html", index.Name))))
 	}
